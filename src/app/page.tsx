059ae6fe01192b075a2f39d516ef960fc13ecbc6
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2024-09-21 16:11:32 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,72 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css';
+import { UploadApiResponse } from 'cloudinary';
+import cloudinary from '@/lib/cloudinary';
+import { redirect } from 'next/navigation';
+import { SubmitButton } from './submit-button';
 
 export default function Home() {
+  const handleSubmit = async (formData: FormData) => {
+    'use server';
+
+    const file = formData.get('video') as File;
+    const aspect_ratio = formData.get('format') as string;
+
+    const buffer: Buffer = Buffer.from(await file.arrayBuffer());
+
+    let url: string = '';
+
+    try {
+      const base64Image: string = `data:${file.type};base64,${buffer.toString(
+        'base64'
+      )}`;
+
+      const uploadResult: UploadApiResponse = await cloudinary.uploader.upload(
+        base64Image,
+        {
+          resource_type: 'video',
+          transformation: [{ aspect_ratio, crop: 'fill' }],
+          public_id: `videos/${Date.now()}`,
+        }
+      );
+
+      url = uploadResult.secure_url;
+    } catch (error: any) {
+      console.error(error);
+    }
+
+    redirect(url);
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className={styles['upload-form']}>
+          <h2>Upload and Resize Your Video</h2>
+          <form action={handleSubmit}>
+            <div className={styles['form-group']}>
+              <label htmlFor='video'>Upload Video:</label>
+              <input type='file' name='video' accept='video/*' required />
+            </div>
+            <div className={styles['form-group']}>
+              <label htmlFor='format'>Formats:</label>
+              <select name='format'>
+                <option value='9:16'>Instagram Reels</option>
+                <option value='1:1'>Instagram Feed</option>
+                <option value='9:16'>TikTok</option>
+                <option value='9:16'>YouTube Shorts</option>
+                <option value='16:9'>YouTube Standard</option>
+                <option value='9:16'>Facebook Stories</option>
+                <option value='1:1'>Facebook Feed</option>
+                <option value='9:16'>Snapchat</option>
+                <option value='1:1'>Twitter/X</option>
+                <option value='1:1'>LinkedIn</option>
+              </select>
+            </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <SubmitButton />
+          </form>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
